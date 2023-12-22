df3bc54248041688449f24b0efaf7180f4e58c4c
Pelpav <elpav2022@gmail.com>
2023-12-22 12:57:20 +0100

    Fin du projet
    Changes to be committed:
    	modified:   Angular/garageapp/angular.json
    	modified:   Angular/garageapp/src/app/app.component.html
    	modified:   Angular/garageapp/src/app/app.component.ts
    	modified:   Angular/garageapp/src/app/app.config.ts
    	new file:   Angular/garageapp/src/app/garage.service.spec.ts
    	new file:   Angular/garageapp/src/app/garage.service.ts
    	modified:   Spring/garageapp/derby.log
    	modified:   Spring/garageapp/src/main/java/com/spring/garageapp/controller/GarageController.java

--- Angular/garageapp/src/app/app.component.ts
+++ Angular/garageapp/src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { GarageService } from './garage.service';
+import { OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [GarageService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'garageapp';
+
+  cars!: any;
+
+  constructor(private garageService: GarageService) {}
+  ngOnInit(): void {
+    console.log('Init');
+    this.garageService.getCars().subscribe((datas) => {
+      this.cars = datas;
+    });
+  }
 }
